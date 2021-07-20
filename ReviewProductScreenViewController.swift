ed3043f83078bdc3c693b99b191aaaec78960faa
Sienna Baird <siennatanner21@icloud.com>
2021-07-20 15:20:50 -0700

    Added the new post (review/ask) view controllers, toolbar/bar button items, your posts view controller, etc

---
+++ ReviewProductScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ReviewProductScreenViewController.swift
+//  SkincareSecretsApp
+//
+//  Created by Sienna Tanner on 7/20/21.
+//
+
+import UIKit
+
+class ReviewProductScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
