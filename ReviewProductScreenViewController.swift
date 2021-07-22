8978a14a2037be7bce4ec8e312dbea64f02ff60e
Sienna Baird <siennatanner21@icloud.com>
2021-07-22 13:43:37 -0700

    added code for the buttons on the product review page

--- ReviewProductScreenViewController.swift
+++ ReviewProductScreenViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class ReviewProductScreenViewController: UIViewController {
 
+    @IBOutlet weak var productReviewTextField: UITextField!
+    
+    @IBOutlet weak var yesOrNoRecommendSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var whyRecommendTextField: UITextField!
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+    @IBOutlet weak var productReviewLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    
+    
+    @IBAction func postProductReviewButtonTapped(_ sender: UIButton) {
+        if let firstProductReviewName = productReviewTextField.text {
+            productNameLabel.text = firstProductReviewName
+        }
+        if let firstProductReview = whyRecommendTextField.text {
+            productReviewLabel.text = firstProductReview
+        
+    }
+    
+    
+    
+    
+    
 
+    
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +54,6 @@ class ReviewProductScreenViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    }
 }
+
